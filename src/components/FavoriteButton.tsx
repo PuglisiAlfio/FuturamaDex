7028c5140a6f69aae42218c315538471c643755b
Alfio Puglisi <puglisi_algio@ciao.it>
2025-07-16 11:26:09 +0200

    aggiunti commenti esplicativi in FavoriteButton.tsx

--- src/components/FavoriteButton.tsx
+++ src/components/FavoriteButton.tsx
@@ -2,35 +2,40 @@ import { useContext } from "react";
 import { FavoriteContext } from "../favorites/FavoritesContext";
 import type { FuturamaCharacter } from "../types";
 
+// Tipo dei props
 type FavoriteButtonProps = {
-  character: FuturamaCharacter;
+  character: FuturamaCharacter; //interfaccia
   className?: string;
 };
 
-export default function FavoriteButton({ character, className = "" }: FavoriteButtonProps) {
-  const favCtx = useContext(FavoriteContext);
-  if (!favCtx) return null;
+export default function FavoriteButton({
+  character,
+  className = "", //personalizzazione classi tramite props
+}: FavoriteButtonProps) {
+  const favCtx = useContext(FavoriteContext); //Recupero il context per gestire i preferiti
+
+  if (!favCtx) return null; // Se il context non è disponibile, non mostro nulla
 
   const { favorites, addToFavorites, removeFromFavorites } = favCtx;
+  const isFavorite = favorites.some((fav) => fav.id === character.id); // Verifico se il personaggio è già tra i preferiti
 
-  const isFavorite = favorites.some((char) => char.id === character.id);
-
+  //Funzione per aggiungere o rimuovere dai preferiti
   const toggleFavorite = (e: React.MouseEvent) => {
     e.stopPropagation();
-    if (isFavorite) {
-      removeFromFavorites(character.id);
-    } else {
-      addToFavorites(character);
-    }
+    return isFavorite
+      ? removeFromFavorites(character.id)
+      : addToFavorites(character);
   };
 
   return (
     <button
       onClick={toggleFavorite}
-      className={`text-2xl cursor-pointer hover:scale-110 transition ${
+      className={`cursor-pointer transition hover:scale-110 text-3xl ${
         isFavorite ? "text-yellow-400" : "text-gray-300"
       } ${className}`}
-      aria-label={isFavorite ? "Rimuovi dai preferiti" : "Aggiungi ai preferiti"}
+      aria-label={
+        isFavorite ? "Rimuovi dai preferiti" : "Aggiungi ai preferiti"
+      }
     >
       ★
     </button>
