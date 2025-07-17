268cc22df33c2a8468770329277ba2d59488d326
Alfio Puglisi <puglisi_algio@ciao.it>
2025-07-17 12:58:10 +0200

    Avvolgiamo l'intere app con il Provider di Redux allo start (main.tsx)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { FavoriteProvider } from "./favorites/FavoritesContext.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <FavoriteProvider>
+    <Provider store={store}>
       <App />
-    </FavoriteProvider>
+    </Provider>
   </StrictMode>
 );
