cfa9c2dabf69c59e1cf6c6cde8fa2aeb1a034061
Alfio Puglisi <puglisi_algio@ciao.it>
2025-07-18 11:34:43 +0200

    Integrazione ToggleThemeButton alla Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
+import ToggleThemeButton from "./ToggleThemeButton";
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-900 text-white p-4 flex gap-4">
+    <nav className="p-4 flex gap-4 bg-gray-200 text-gray-900 dark:bg-gray-900 dark:text-white">
       <Link to="/" className="hover:underline">
         Home
       </Link>
       <Link to="/favorites" className="hover:underline">
         Favoriti
       </Link>
+      <ToggleThemeButton />
     </nav>
   );
 }
